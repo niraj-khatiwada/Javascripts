92550bb5b0a78ee1841c219916dafbd9f607766f
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-03-25 23:45:46 +0545

    Events

--- Events/main.js
+++ Events/main.js
@@ -4,11 +4,22 @@
 
 
 const btn = document.querySelector("button")
-btn.addEventListener('click', () => document.querySelector("h1").style.color = "red")
-btn.addEventListener("click", () => btn.innerText = "Stop touching me")
+// btn.addEventListener('click', () => document.querySelector("h1").style.color = "red")
+// btn.addEventListener("click", () => btn.innerText = "Stop touching me")
 
-const input = document.querySelector("input")
-input.addEventListener("mouseover", () => btn.style.border = "3px solid red")
-input.addEventListener('mouseout', () => btn.style = "none")
+// const input = document.querySelector("input")
+// input.addEventListener("mouseover", () => btn.style.border = "3px solid red")
+// input.addEventListener('mouseout', () => btn.style = "none")
 
-window.addEventListener('scroll', () => document.querySelector("p").style.fontSize = "3rem")
+// window.addEventListener('scroll', () => document.querySelector("p").style.fontSize = "3rem")
+
+
+btn.addEventListener("mouseover", function () {
+    // const width = Math.ceil(Math.random()*window.innerWidth);
+    // const height = Math.ceil(Math.random()*window.innerHeight);
+    btn.style.transform = `translate(100px,200px)`;
+
+})
+btn.addEventListener("click", function () {
+    btn.style.background = "green"
+})
