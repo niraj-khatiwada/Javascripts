49b06b38d85b48a0eec059fe1a01b1a8850d51fc
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-03-25 17:46:38 +0545

    Important methods

--- JS_HTML/main.js
+++ JS_HTML/main.js
@@ -19,12 +19,46 @@
 // }
 
 
+//Display value of input range
 const range = document.querySelector("input[type = 'range']")
 
 const displayRangeValue = document.querySelector(".display_range_value")
 displayRangeValue.textContent = range.value
 
-
+//Classlist
 const todos = document.querySelector(".todo")
 todos.classList.toggle('done')
 
+//Add element Append and Prepend
+const beerDiv = document.querySelector(".beer")
+
+const newH2 = document.createElement('h2')
+newH2.innerText = "Beer"
+newH2.style.textAlign = "center"
+newH2.style.color = "red"
+
+beerDiv.append(newH2)
+
+//Insert Before
+const liCoffee = document.querySelectorAll(".todo")
+const liCoffee_parent = document.querySelector(".todos")
+const li_tea = document.createElement("li")
+li_tea.innerText = "Tea"
+
+liCoffee_parent.insertBefore(li_tea, liCoffee[1])
+
+
+//Before Begin, After Begin, Before End, After End
+
+const p = document.querySelector("#main")
+
+const newH1 = document.createElement('h1')
+newH1.textContent = "Niraj Khatiwada"
+p.insertAdjacentElement("beforeend", newH1 )
+
+
+//Remove element
+
+const rem_ul = document.querySelector(".fruits")
+
+rem_ul.remove()
