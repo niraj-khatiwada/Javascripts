f0140289155ad26f132beb0a5489a21a352393ff
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-03-25 18:05:05 +0545

    Golden State Vs Houston NBA

---
+++ NBA/JS/app.js
@@ -0,0 +1,64 @@
+const warriorGames = [
+    {
+        awayteam: {
+            team: 'Golden State',
+            points: 119,
+            isWinner: true
+        },
+        hometeam: {
+            team: 'Houston',
+            points: 106,
+            isWinner: false
+        }
+    },
+    {
+        awayteam: {
+            team: 'Golden State',
+            points: 105,
+            isWinner: false
+
+        },
+        hometeam: {
+            team: 'Housten',
+            points: 127,
+            isWinner: true
+        },
+    },
+    {
+        awayteam: {
+            team: 'Houston',
+            points: 85,
+            isWinner: false
+        },
+        hometeam: {
+            team: 'Golden State',
+            points: 126,
+            isWinner: true
+        }
+    },
+    {
+        hometeam: {
+            team: 'Golden State',
+            points: 115,
+            isWinner: true
+        },
+        awayteam: {
+            team: 'Houston',
+            points: 86,
+            isWinner: false
+        }
+    },
+    {
+        awayteam: {
+            team: 'Golden State',
+            points: 101,
+            isWinner: true
+        },
+        hometeam: {
+            team: 'Houston',
+            points: 92,
+            isWinner: false
+        }
+    }
+]
+
