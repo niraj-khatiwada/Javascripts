7f6cf573f7b7ec9adcd1b1b180baba589a47ce40
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-03-26 12:56:51 +0545

    Keypress Events

--- Events/main.js
+++ Events/main.js
@@ -28,37 +28,73 @@
 
 const colors = ["red", "green", "blue", "teal", "yellow", "maroon", "pink", "orange", "purple"]
 
-const itemBoxes = () => {
-    const div_ = document.createElement("div")
-    document.body.appendChild(div_)
-    div_.addEventListener("click",function () {
-        printCoroinates(div_)
+// const itemBoxes = () => {
+//     const div_ = document.createElement("div")
+//     document.body.appendChild(div_)
+//     div_.addEventListener("click",function (evt) {
+//         console.log(evt)
+//         printCoroinates(div_, evt)
         
-    })
-    div_.style.width = "500px"
-    div_.style.background = "blue"
+//     })
+//     div_.style.width = "500px"
+//     div_.style.background = "blue"
     
-    for (let color of colors) {
-        const div = document.createElement("div")
-        div.style.width = "100px"
-        div.style.height = "100px"
-        div.style.background = `${color}`
-        document.body.appendChild(div)
+//     for (let color of colors) {
+//         const div = document.createElement("div")
+//         div.style.width = "100px"
+//         div.style.height = "100px"
+//         div.style.background = `${color}`
+//         document.body.appendChild(div)
+//     }
+// }
+
+// const pickColor = function (evt) {
+//     const pickedColor = document.querySelector("h1"); 
+//     pickedColor.style.color = `${this.style.background}`;
+//     console.log(evt.screenX, evt.screenY)
+// }
+
+// const printCoroinates = function (div_, evt) {
+//     const button = document.createElement("button")
+//     button.style.position ="absolute"
+//     button.top = evt.screenX
+//     button.left = evt.screenY
+//     button.innerText = `(${evt.screenX}, ${evt.screenY})`
+//     div_.appendChild(button)
+    
+// }
+// itemBoxes()
+
+const colorBoxes = function () {
+    const mainDiv = document.createElement("div")
+    mainDiv.style.display = "flex"
+
+    document.body.appendChild(mainDiv)
+    for (let item of colors) {
+        const childDiv = document.createElement("div")
+        childDiv.style.backgroundColor = item
+        childDiv.className = "childDiv"
+        childDiv.style.width = "100px"
+        childDiv.style.height = "100px"
+        mainDiv.appendChild(childDiv)
     }
-}
-
-const pickColor = function (evt) {
-    const pickedColor = document.querySelector("h1"); 
-    pickedColor.style.color = `${this.style.background}`;
-    console.log(evt.screenX, evt.screenY)
-}
-
-const printCoroinates = function (div_, evt) {
-    const button = document.createElement("button")
-    button.style.position ="absolute"
-    button.top = evt.screenX
-    button.left = evt.screenY
-    div_.appendChild(button)
     
+};
+
+const spacePressed = function () {
+    const _childDiv = document.querySelectorAll(".childDiv");
+    console.log(_childDiv)
+    document.body.addEventListener("keypress", function (evt) {
+        if (evt.code === "Space") {
+            const H1 = document.querySelector("h1")
+            const randomColor = colors[Math.ceil(Math.random() * colors.length)]
+            H1.style.color = randomColor
+
+        }
+    })
 }
-itemBoxes()
+
+
+
+colorBoxes()
+spacePressed()
