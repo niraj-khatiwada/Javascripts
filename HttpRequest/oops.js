36f37f3e0a4c47a1a9ba90ad321395043712da4a
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-03-27 15:14:46 +0545

    Parallel async

---
+++ HttpRequest/oops.js
@@ -0,0 +1,42 @@
+// const student = function (name, roll, gen) {
+//     details = {}
+//     details.name = name
+//     details.roll = roll
+//     details.gen = gen
+
+//     const printDetails = () =>  {
+//         console.log(`${name} of roll number ${roll} is ${gen}`)
+//     }
+// }
+
+
+
+// const Student = function (name, roll, gen) {
+//     this.name = name
+//     this.roll = roll
+//     this.gen = gen  
+// }
+
+// // Student.prototype.printDetails
+// const obj =new Student("Niraj Khatiwada", 13, "Male")
+
+// Student.prototype.printDetails = function () {
+//     console.log(`${this.name} of roll number ${this.roll} is ${this.gen}`)
+// }
+
+// obj.printDetails()
+
+
+class Student {
+    constructor() {
+        
+    }
+
+    printDetails (name, roll, gen) {
+        console.log(`${name} of roll number ${roll} is ${gen}`)
+    }
+}
+
+
+const niraj = new Student()
+niraj.printDetails("Niraj Khatiwada", 13, "Male")
---
+++ HttpRequest/protoypes.js
@@ -0,0 +1,18 @@
+// String.prototype.slice = () => {
+//     return alert("hello world")
+// }
+
+// Array.prototype.pop = () => {
+//     return alert("popped")
+// }
+
+
+function changeCase(value) {
+    return value.toUpperCase()
+}
+
+String.prototype.hello = () => {
+    return alert("hello")
+}
+
+String.prototype.splice = function () {return alert("Hello")}
