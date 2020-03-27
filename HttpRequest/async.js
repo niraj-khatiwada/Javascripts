288901623281abcf4542627648031708370d61ef
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-03-27 11:04:58 +0545

    Async Functions

--- HttpRequest/async.js
+++ HttpRequest/async.js
@@ -1,32 +1,15 @@
-// async function add(x, y) {
-//     if (typeof x != 'number' || typeof y != 'number'){
-//         throw new Error("Not a number")
-//     }
-//     return x+y
-// }
 
-// add('e', 4).then((res) => {
-//     console.log("Success",res )
-// })
-// .catch((err) => {
-//     console.log("Failed", err)
-// })
-
-
-function mil(x, y) {
-    return new Promise((resolve, reject) => {
-        if (typeof x != 'number' || typeof y != 'number'){
-            reject("Not a number")
-        }
-        resolve(x+y)
-    })
+async function getURL() {
+    try {
+        const res = await axios.get("https://swapi.co/api/planets");
+        const residents  = await axios.get(`res.data.results[0].residents[0] ${as}`)
+        console.log(residents.data.name)
+        
+    }
+    catch (err) {
+        console.log("errorahjsjbhashb ", err)
+    } 
+    
 }
 
-mil(1, 'e')
-.then((res) => {
-    console.log("Resolved: ", res)
-
-})
-.catch((err) => {
-    console.log("Failed", err)
-})
+getURL()
