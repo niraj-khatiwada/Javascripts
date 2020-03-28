b30444cfded80c9296b65cf427570f91f1f781aa
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-03-28 11:38:53 +0545

    Reduce function is great

--- JS/practice.js
+++ JS/practice.js
@@ -170,78 +170,100 @@ const votes = ['y', 'y', 'n', 'n', 'y', 'n', 'y', 'y', 'y', 'n', 'n', 'y', 'n']
 // console.log(results)
 
 
-const books = [{
-    title: 'Good Omens',
-    authors: ['Terry Pratchett', 'Neil Gaiman'],
-    rating: 4.25,
-    genres: ['fiction', 'fantasy']
-},
-{
-    title: 'Changing My Mind',
-    authors: ['Zadie Smith'],
-    rating: 3.83,
-    genres: ['nonfiction', 'essays']
-},
-{
-    title: 'Bone: The Complete Edition',
-    authors: ['Jeff Smith'],
-    rating: 4.42,
-    genres: ['fiction', 'graphic novel', 'fantasy']
-},
-{
-    title: 'American Gods',
-    authors: ['Neil Gaiman'],
-    rating: 4.11,
-    genres: ['fiction', 'fantasy']
-},
-{
-    title: 'A Gentleman in Moscow',
-    authors: ['Amor Towles'],
-    rating: 4.36,
-    genres: ['fiction', 'historical fiction']
-},
-{
-    title: 'The Name of the Wind',
-    authors: ['Patrick Rothfuss'],
-    rating: 4.54,
-    genres: ['fiction', 'fantasy']
-},
-{
-    title: 'The Overstory',
-    authors: ['Richard Powers'],
-    rating: 4.19,
-    genres: ['fiction', 'short stories']
-},
-{
-    title: 'A Truly Horrible Book',
-    authors: ['Xavier Time'],
-    rating: 2.18,
-    genres: ['fiction', 'garbage']
-},
-{
-    title: 'The Way of Kings',
-    authors: ['Brandon Sanderson'],
-    rating: 4.65,
-    genres: ['fantasy', 'epic']
-},
-{
-    title: 'Lord of the flies',
-    authors: ['William Golding'],
-    rating: 3.67,
-    genres: ['fiction']
-}
-]
+// const books = [{
+//     title: 'Good Omens',
+//     authors: ['Terry Pratchett', 'Neil Gaiman'],
+//     rating: 4.25,
+//     genres: ['fiction', 'fantasy']
+// },
+// {
+//     title: 'Changing My Mind',
+//     authors: ['Zadie Smith'],
+//     rating: 3.83,
+//     genres: ['nonfiction', 'essays']
+// },
+// {
+//     title: 'Bone: The Complete Edition',
+//     authors: ['Jeff Smith'],
+//     rating: 4.42,
+//     genres: ['fiction', 'graphic novel', 'fantasy']
+// },
+// {
+//     title: 'American Gods',
+//     authors: ['Neil Gaiman'],
+//     rating: 4.11,
+//     genres: ['fiction', 'fantasy']
+// },
+// {
+//     title: 'A Gentleman in Moscow',
+//     authors: ['Amor Towles'],
+//     rating: 4.36,
+//     genres: ['fiction', 'historical fiction']
+// },
+// {
+//     title: 'The Name of the Wind',
+//     authors: ['Patrick Rothfuss'],
+//     rating: 4.54,
+//     genres: ['fiction', 'fantasy']
+// },
+// {
+//     title: 'The Overstory',
+//     authors: ['Richard Powers'],
+//     rating: 4.19,
+//     genres: ['fiction', 'short stories']
+// },
+// {
+//     title: 'A Truly Horrible Book',
+//     authors: ['Xavier Time'],
+//     rating: 2.18,
+//     genres: ['fiction', 'garbage']
+// },
+// {
+//     title: 'The Way of Kings',
+//     authors: ['Brandon Sanderson'],
+//     rating: 4.65,
+//     genres: ['fantasy', 'epic']
+// },
+// {
+//     title: 'Lord of the flies',
+//     authors: ['William Golding'],
+//     rating: 3.67,
+//     genres: ['fiction']
+// }
+// ]
 
-const results = books.reduce((preVal, currVal) => {
-    const rating =Math.floor(currVal.rating)
-    if (preVal[rating]){
-        preVal[rating].push(currVal)
-    }
-    else{
-        preVal[rating] = []
-    }
+// const results = books.reduce((preVal, currVal) => {
+//     const rating =Math.floor(currVal.rating)
+//     if (preVal[rating]){
+//         preVal[rating].push(currVal)
+//     }
+//     else{
+//         preVal[rating] = []
+//     }
    
-    return preVal
-}, {})
+//     return preVal
+// }, {})
 
-console.log(results)
+// console.log(results)
+
+// const arr = [100, 1, 24, 15, 10, 2, 5, 23, 1, 0, 19]
+// console.log(array.sort((x, y) => {
+//     return x-y
+// }))
+
+// const sorted = arr.reduce((preVal, currVal) => {
+//     if (preVal.length === 0){
+//         preVal.push(currVal)
+//     }
+//     else{
+//         if (preVal[preVal.length - 1] > currVal){
+//             arr.splice(arr.indexOf(preVal), 1, preVal)
+//         }
+
+//     }
+//     return preVal
+// }, [])
+
+// console.log(sorted)
+
+//[100]1
